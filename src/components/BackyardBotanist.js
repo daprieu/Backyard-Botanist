50306a28d305ae30c214fc4338a7e2c3cd707b30
daprieu <daprieu@gmail.com>
2021-03-15 14:14:12 -0500

    Imported Auth files

---
+++ src/components/BackyardBotanist.js
@@ -0,0 +1,32 @@
+// import logo from './logo.svg';
+// import './App.css';
+import { Route, Redirect } from "react-router-dom"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import { userStorageKey } from "./auth/authSettings"
+
+export const BackyardBotanist = () => (
+    <>
+    <Route render={() => {
+      if (sessionStorage.getItem(userStorageKey)) {
+        return (
+          <>
+            //Components that are rendered when the user is authenticated go inside this React fragment
+          </>
+        )
+      } else {
+        return <Redirect to="/login" />;
+      }
+  }} />
+
+  <Route path="/login">
+    <Login />
+  </Route>
+  <Route path="/register">
+    <Register />
+  </Route>
+  </>
+  );
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router } from "react-router-dom"
 import './index.css';
-import App from './App';
+import { BackyardBotanist } from './components/BackyardBotanist';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <BackyardBotanist />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
