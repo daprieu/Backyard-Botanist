dd6a128a5cd7440fd95792bbdafc08edf499ea4c
daprieu <daprieu@gmail.com>
2021-03-16 09:24:50 -0500

    added AppViews for future and removed console.log in userProv

---
+++ src/components/AppViews.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Route } from "react-router-dom"
+
+export const AppViews = () => {
+    return (
+        <>
+            <Route exact path="/">
+                
+            </Route>
+        </>
+    )
+}
