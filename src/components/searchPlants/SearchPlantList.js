95d100da861920891c8490b3578f294c05b41364
daprieu <daprieu@gmail.com>
2021-03-17 14:26:46 -0500

    added more comments

--- src/components/searchPlants/SearchPlantList.js
+++ src/components/searchPlants/SearchPlantList.js
@@ -10,6 +10,9 @@ export const SearchablePlantList = () => {
     const [filteredPlants, setFiltered] = useState([])
     console.log('filteredPlants: ', filteredPlants);
     
+
+    // the first useEffect takes the fetchcall function and passes the searchterms from the 
+    // search bar to it
     useEffect(() => {
         
         if (searchTerms !== "") {
@@ -21,13 +24,17 @@ export const SearchablePlantList = () => {
     
     }, [searchTerms])
 
+    // the second useEffect takes the searchablePlants that has been updated by the getSearchablePlants
+    //and sets the filteredPlants array with the new information
+    // when there are no search terms available the filteredPlants is just set 
+    // to and empty array
     useEffect(() => {
         if (searchTerms !== "") {
             const results = searchablePlants
             
             setFiltered(results)
         } else {
-            setFiltered(searchablePlants)
+            setFiltered([])
         }
     }, [searchTerms, searchablePlants])
 
