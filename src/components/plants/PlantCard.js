88070f3f460edfb683688bcacad33563f5aee390
daprieu <daprieu@gmail.com>
2021-03-22 10:07:46 -0500

    finished my app styling

--- src/components/plants/PlantCard.js
+++ src/components/plants/PlantCard.js
@@ -2,16 +2,23 @@ import React from "react"
 import { Link } from "react-router-dom"
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
 
-
+const styles = {
+cardImage: {
+    objectFit: 'cover',
+    borderRadius: 25,
+    height: '16rem'
+  }
+}
 
 export const PlantCard = ({ plant }) => {
     
     return (
         
         
-        <Card style={{ width: '14rem' }}>
-        <Card.Img variant="top" src={plant.image}  />
+        <Card style={{ width: '14rem', borderRadius: 25}}>
+        <Card.Img variant="top" src={plant.image} style={styles.cardImage} />
         <Card.Body>
           <Card.Title>{plant.scientificName}</Card.Title>
           <Card.Text>{ plant.commonName }</Card.Text>
--- src/components/searchPlants/SearchPlantCard.js
+++ src/components/searchPlants/SearchPlantCard.js
@@ -3,13 +3,21 @@ import { Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import { Button } from "react-bootstrap"
 
+const styles = {
+    cardImage: {
+        objectFit: 'cover',
+        borderRadius: 25,
+        height: '16rem'
+      }
+    }
+
 export const SearchPlantCard = ({plant}) => (
-    <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src={plant.image_url} size="20%" />
+    <Card style={{ width: '18rem', borderRadius: 25 }}>
+    <Card.Img variant="top" src={plant.image_url} style={styles.cardImage}/>
     <Card.Body >
     <Card.Text>{ plant.scientific_name }</Card.Text>
     <Card.Text>{ plant.common_name }</Card.Text>
-    <Link to={`/search/addplant/${plant.id}`}><Button variant="success">
+    <Link to={`/search/addplant/${plant.id}`}><Button variant="primary">
               Add Plant</Button></Link>
     </Card.Body>
     </Card>
