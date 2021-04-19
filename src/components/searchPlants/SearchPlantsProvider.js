a0799e77d0b275ce65a8d5bddc18bbdfcf112962
daprieu <daprieu@gmail.com>
2021-04-19 13:04:49 -0500

    removed ./ to settings

--- src/components/searchPlants/SearchPlantsProvider.js
+++ src/components/searchPlants/SearchPlantsProvider.js
@@ -1,5 +1,5 @@
 import React, { useState, createContext } from "react"
-import { trefleAPI } from "./../../Settings"
+import { trefleAPI } from "../../Settings"
 
 export const SearchPlantsContext = createContext()
 
